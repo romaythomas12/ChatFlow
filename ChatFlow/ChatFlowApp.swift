ebb9a2ae9de31c4245c2b5a6b39483a0f65e5788
Thomas Romay <romaythomas@gmail.com>
2024-11-03 22:06:47 +0000

    Initial Commit

---
+++ ChatFlow/ChatFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatFlowApp.swift
+//  ChatFlow
+//
+//  Created by Thomas Romay on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatFlowTests/ChatFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatFlowTests.swift
+//  ChatFlowTests
+//
+//  Created by Thomas Romay on 03/11/2024.
+//
+
+import Testing
+
+struct ChatFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
